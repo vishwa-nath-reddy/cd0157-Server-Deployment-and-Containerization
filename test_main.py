8a6811934e7577d174bc76fc9d4e28ea221ebcad
vishwanath <vishwa.nath.reddy.021@gmail.com>
2023-05-03 23:32:03 +0530

    updated files

--- test_main.py
+++ test_main.py
@@ -38,3 +38,56 @@ def test_auth(client):
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+
+# import json
+import unittest
+from main import APP
+
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        APP.testing = True
+        self.client = APP.test_client()
+
+    def test_health(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data), "Healthy")
+
+    def test_auth_missing_email(self):
+        data = {'password': 'mypassword'}
+        response = self.client.post('/auth', json=data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_auth_missing_password(self):
+        data = {'email': 'user@example.com'}
+        response = self.client.post('/auth', json=data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_auth_valid_credentials(self):
+        data = {'email': 'user@example.com', 'password': 'mypassword'}
+        response = self.client.post('/auth', json=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(json.loads(response.data)['token'])
+
+    def test_decode_jwt_no_auth_header(self):
+        response = self.client.get('/contents')
+        self.assertEqual(response.status_code, 401)
+
+    def test_decode_jwt_invalid_token(self):
+        headers = {'Authorization': 'Bearer invalidtoken'}
+        response = self.client.get('/contents', headers=headers)
+        self.assertEqual(response.status_code, 401)
+
+    def test_decode_jwt_valid_token(self):
+        data = {'email': 'user@example.com', 'password': 'mypassword'}
+        auth_response = self.client.post('/auth', json=data)
+        token = json.loads(auth_response.data)['token']
+        headers = {'Authorization': f'Bearer {token}'}
+        response = self.client.get('/contents', headers=headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data)['email'], 'user@example.com')
+
+if __name__ == "__main__":
+    unittest.main()
